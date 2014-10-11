b4e3fa174705567c2b6024bcadfaf7d6777afa7e
wun peng <ah-peng@hotmail.com>
2014-10-11 14:45:39 -0700

    seperate file to part b and c

--- Part C -PEAR DB/index.php
+++ Part C -PEAR DB/index.php
@@ -8,10 +8,30 @@
 <body>
 
 <h1>Search your wine here</h1>
-<form action="result.php" method="GET">
+<?php include_once("connect.php"); ?>
+<form action="result.php" method="get">
 <table border="1" cellpadding="2">
 <tr><td>Wine name: </td><td><input type="text" name="wine_name"></td></tr>
-<tr><td>Wine Region: </td><td><input type="text" name="wine_region"></td></tr>
+<tr><td>Wine Region: </td><td>
+<select name="wine_region">
+
+<?php 
+// select region name from region table
+$query="SELECT * FROM region";
+if (!($result = mysql_query ( $query,$connection)))
+      showerror();
+	  $temp=mysql_fetch_array($result);
+	  
+	  while($temp=mysql_fetch_array($result)){
+		  echo "<option value='".$temp["region_name"]."''>";
+		  echo $temp["region_name"];
+		  echo "</option>\n";
+		  
+		  
+		  }
+	  mysql_close($connection);
+?>
+</select></td></tr>
 <tr><td>Winery name: </td><td><input type="text" name="winery_name"></td></tr>
 <tr><td>Wine years:  </td><td>From: <input type="text" name="wine_yearMin"></td><td>To:<input type="text" name="wine_yearMax"></td></tr>
 <tr><td>Minumum number of wines in stock: </td><td><input type="text" name="wine_quantity"></td></tr>
