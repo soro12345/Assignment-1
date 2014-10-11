4ff57bfb0a89e11d76eda32479de3438b422a391
wun peng <ah-peng@hotmail.com>
2014-10-10 17:15:01 -0700

    connect.php
    a connect.php that make convenient connection

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+		
+    // Show error function    
+   function showerror()
+   {
+      die("Error " . mysql_errno() . " : " . mysql_error());
+   }
+	
+   // (1) Open the database connection
+   if (!($connection = @ mysql_connect("localhost","root")))
+      die("Could not connect");
+
+   // (2) Select the winestore database
+   if (!mysql_select_db("winestore", $connection))
+      showerror();
+?>
